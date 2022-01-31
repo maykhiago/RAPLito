01835034d17f38ee955e1a2f46206f475e5815e8
HiagoMayk <HiagoMayk>
2022-01-31 18:07:01 -0300

    ok

---
+++ rapl_amd.cpp
@@ -0,0 +1,105 @@
+/* File that contains the variable declarations */
+#include "rapl.h"
+#include <stdio.h>
+
+/*global variables*/
+
+static int package_map[MAX_PACKAGES];
+char packname[MAX_PACKAGES][256];
+char tempfile[256];
+double initGlobalTime = 0.0;
+unsigned long int idKernels[MAX_KERNEL];
+short int id_actual_region=0;
+short int auroraMetric;
+short int totalKernels=0;
+short int auroraTotalPackages=0;
+short int auroraTotalCores=0;
+
+typeFrame auroraKernels[MAX_KERNEL];
+
+/*-----------------------------*/
+long long parcial[MAX_READ][MAX_PACKAGES][NUM_RAPL_DOMAINS];
+short int read_count_energy = 1;
+short int leituras = 0;
+/*---------------------------*/
+
+void detect_packages()
+{
+	char filename[STRING_BUFFER];
+	FILE *fff;
+	int package;
+	int i;
+
+	for(i=0;i<MAX_PACKAGES;i++)
+    package_map[i]=-1;
+
+	for(i=0;i<MAX_CPUS;i++)
+  {
+		sprintf(filename,"/sys/devices/system/cpu/cpu%d/topology/physical_package_id",i);
+		fff=fopen(filename,"r");
+
+		if (fff==NULL)
+      break;
+
+		fscanf(fff,"%d",&package);
+		fclose(fff);
+
+		if (package_map[package]==-1)
+    {
+			auroraTotalPackages++;
+			package_map[package]=i;
+		}
+	}
+}
+
+void start_rapl_sysfs()
+{
+	char msr_filename[STRING_BUFFER];
+	int fd;
+
+	sprintf(msr_filename, "/dev/cpu/0/msr");
+	fd = open(msr_filename, O_RDONLY);
+	if ( fd < 0 )
+  {
+		if ( errno == ENXIO )
+    {
+			fprintf(stderr, "rdmsr: No CPU 0\n");
+			exit(2);
+		}
+    else if ( errno == EIO )
+    {
+			fprintf(stderr, "rdmsr: CPU 0 doesn't support MSRs\n");
+			exit(3);
+		}
+    else
+    {
+			perror("rdmsr:open");
+			fprintf(stderr,"Trying to open %s\n",msr_filename);
+			exit(127);
+		}
+	}
+	uint64_t data;
+	pread(fd, &data, sizeof data, AMD_MSR_PACKAGE_ENERGY);
+	//auroraKernels[id_actual_region].kernelBefore[0] = read_msr(fd, AMD_MSR_PACKAGE_ENERGY);
+	auroraKernels[id_actual_region].kernelBefore[0] = (long long) data;
+}
+
+double end_rapl_sysfs(){
+	char msr_filename[STRING_BUFFER];
+        int fd;
+        sprintf(msr_filename, "/dev/cpu/0/msr");
+        fd = open(msr_filename, O_RDONLY);
+		uint64_t data;
+        pread(fd, &data, sizeof data, AMD_MSR_PWR_UNIT);
+	int core_energy_units = (long long) data;
+	unsigned int energy_unit = (core_energy_units & AMD_ENERGY_UNIT_MASK) >> 8;
+	pread(fd, &data, sizeof data, AMD_MSR_PACKAGE_ENERGY);
+	auroraKernels[id_actual_region].kernelAfter[0] = (long long) data;
+	double result = (auroraKernels[id_actual_region].kernelAfter[0] - auroraKernels[id_actual_region].kernelBefore[0])*pow(0.5,(float)(energy_unit));
+	return result;
+}
+
+void rapl_init()
+{
+        detect_packages();
+}
---
+++ rapl_amd.h
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <math.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <linux/perf_event.h>
+#include <unistd.h>
+#include <stdint.h>
+
+/*define AMD_MSR ENVIRONMENT*/
+
+#define AMD_MSR_PWR_UNIT 0xC0010299
+#define AMD_MSR_CORE_ENERGY 0xC001029A
+#define AMD_MSR_PACKAGE_ENERGY 0xC001029B
+#define AMD_TIME_UNIT_MASK 0xF0000
+#define AMD_ENERGY_UNIT_MASK 0x1F00
+#define AMD_POWER_UNIT_MASK 0xF
+#define STRING_BUFFER 1024
+#define MAX_CPUS                128 //1024
+#define MAX_PACKAGES            4 //16
+
+
+/*define AURORA environment*/
+
+#define MAX_KERNEL              61
+#define MAX_THREADS             32
+#define PERFORMANCE             0
+#define ENERGY                  1
+#define EDP                     2
+
+
+#define END                     10
+#define S0                      0
+#define S1                      1
+#define S2                      2
+#define S3                      3
+#define REPEAT                  4
+
+typedef struct{
+        short int numThreads;
+        short int numCores;
+        short int bestThread;
+        short int auroraMetric;
+        short int state;
+        short int lastThread;
+        short int startThreads;
+	      int steps;
+        short int pass;
+        double bestResult, initResult, lastResult, bestTime, total_region_perf;
+        long long kernelBefore[MAX_PACKAGES];
+        long long kernelAfter[MAX_PACKAGES];
+
+}typeFrame;
+
+//Methods
+void rapl_init(void);
+void detect_packages(void);
+void start_rapl_sysfs(void); //AMD aurora_start_amd_msr()
+double end_rapl_sysfs(void); //AMD aurora_end_amd_msr()
+
+/*-----------------------------*/
+void ALARMhandler(int);
+double end_rapl_parcial_reading();
+/*---------------------------*/
